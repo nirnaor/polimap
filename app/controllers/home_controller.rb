f56f4f6da1983aa3ea8262b67fdff38acd1d6fa8
Nir Naor <nirnaor@Nirs-MacBook-Air.local>
2015-02-05 12:17:58 +0200

    Added members method.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
 class HomeController < ApplicationController
   def index
+    gon.members = members_json
+  end
+
+
+  def members_json
     res = []
     Member.all.each do |member|
       res << member.as_json(:include => :parties).merge(:city => member.city.as_json)
     end
-    gon.members = res.to_json
+    res.to_json
   end
 end
