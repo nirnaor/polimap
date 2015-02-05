a1d194dcfceddbfd3ae54f7767703210a7f06a8d
Nir Naor <nirnaor@Nirs-MacBook-Air.local>
2015-02-05 12:35:57 +0200

    Passing JSON votes.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     gon.members = members_json
+    gon.votes = votes_json
   end
 
 
@@ -8,6 +9,14 @@ class HomeController < ApplicationController
     res = []
     Member.all.each do |member|
       res << member.as_json(:include => :parties).merge(:city => member.city.as_json)
+    end
+    res.to_json
+  end
+
+  def votes_json
+    res = []
+    Vote.all.each do |vote|
+      res << vote.as_json(:include => :party).merge(:city => vote.city.as_json)
     end
     res.to_json
   end
