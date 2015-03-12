26673cb06b3a591f1a72e6eaf3287fae6b26b143
Nir Naor <nirnaor@Nirs-MacBook-Air.local>
2015-03-12 11:53:44 +0200

    Fixed DB issues.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150205092255) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cities", force: true do |t|
     t.string   "name"
     t.float    "lat"
